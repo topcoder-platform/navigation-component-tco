6d7e0cbd9befefcf82d18793e914200f0da9c5e7
Kiril Kartunov <kiril.kartunov@gmail.com>
2021-07-07 12:34:55 +0300

    back to anchors for mobile navi

--- src/components/TopNav/MobileMenu.js
+++ src/components/TopNav/MobileMenu.js
@@ -40,14 +40,14 @@ const MobileMenu = ({
           if ((level2.subMenu && level2.subMenu.length > 0) || level2.href) {
             return (
               <>
-                <Link
+                <a
                   className={cn(styles.secondaryNavMobileItem, level2.id === activeChildId && styles.secondaryNavMobileItemOpen, level2.subMenu && styles.hasSubMenu)}
-                  to={!_.get(level2, 'subMenu.length', 0) && level2.href}
+                  href={!_.get(level2, 'subMenu.length', 0) && level2.href}
                   key={`level2-${i}`}
                   onClick={level2.subMenu && level2.subMenu.length > 0 ? createHandleClickItem(level2.id) : null}
                 >
                   {renderItem(level2)}
-                </Link>
+                </a>
                 {level2.subMenu && level2.id === activeChildId && getSubMenu(level2.subMenu)}
               </>
             )
